32117ce514e8ab35fb776d3fa4db3213e5c0f6c3
john-dring <45520007+john-dring@users.noreply.github.com>
2018-12-04 18:50:43 -0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,14 +7,14 @@ class item:
     self.val = valn #0,1,2,3
     self.time = time #timestamp used for program syncing
   def __str__:
-    return "{}:{}".format(self.val,self.time)
+    return "{}:{} ".format(self.val,self.time)
     
 
 while true:
   sleep(0.1)
   events = joystick.get_events
   for event in events:
-    if event.val == 5 or not event.down:
+    if event.val >= 4 or not event.down:
       exit()
     i = item(event.val, time.time())
     f = open("userimput.csv","w")
