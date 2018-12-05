8fe1ae6a1ca5ccff18326320a8fdb74c06afb12f
john-dring <45520007+john-dring@users.noreply.github.com>
2018-12-04 18:23:37 -0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,1 +1,26 @@
+import pygame
+import joystick
+import time
 
+class item:
+  def __init__(self,val,time):
+    self.val = valn #0,1,2,3
+    self.time = time #timestamp used for program syncing
+  def __str__:
+    return "{},{}".format(self.val,self.time)
+    
+
+while true:
+  sleep(0.1)
+  events = joystick.get_events
+  for event in events:
+    if event.val == 5 or not event.down:
+      exit()
+    i = item(event.val, time.time())
+    f = open("userimput.txt","w")
+    f.write(i)
+    
+    
+  
+  
+  
